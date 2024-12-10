fbf7a8722fb02d249caaa7b44698c98fdd87153c
Olesea0907 <olesea.grosu@stud.ubbcluj.ro>
2024-12-10 04:37:25 +0200

    . punctul 1-5

---
+++ Models/ListProduct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace Grosu_Olesea_Lab7.Models
+{
+    public class ListProduct
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        // Relația cu ShopList
+        [ForeignKey(typeof(ShopList))]
+        public int ShopListID { get; set; }
+
+        // Relația cu Product
+        [ForeignKey(typeof(Product))]
+        public int ProductID { get; set; }
+    }
+}
+
---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+using System.Collections.Generic;
+
+namespace Grosu_Olesea_Lab7.Models
+{
+    public class Product
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        public string Description { get; set; }
+
+        [OneToMany(CascadeOperations = CascadeOperation.All)]
+        public List<ListProduct> ListProducts { get; set; }
+    }
+}
